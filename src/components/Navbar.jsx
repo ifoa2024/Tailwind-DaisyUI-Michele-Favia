a3ef91ba1575b537c34fdde0e8178116de92eeba
zNicK99 <nicolasfortunato1964@gmail.com>
2025-01-22 12:54:30 +0100

    dark mode icona

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MdOutlineDarkMode } from 'react-icons/md';
 
 const Navbar = ({ toggleTheme, isDarkMode }) => {
   return (
@@ -8,16 +9,19 @@ const Navbar = ({ toggleTheme, isDarkMode }) => {
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a>Home</a></li>
+          <li><a>About</a></li>
+          <li><a>Contact</a></li>
         </ul>
-        <button className="btn btn-primary ml-4" onClick={toggleTheme}>
-          {isDarkMode ? 'Tema Chiaro' : 'Tema Scuro'}
-        </button>
+        {/* Icona al posto del bottone */}
+        <MdOutlineDarkMode
+          className="text-2xl cursor-pointer"
+          onClick={toggleTheme}
+        />
       </div>
     </nav>
   );
 };
 
 export default Navbar;
+
